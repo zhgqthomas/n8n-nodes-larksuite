b16ae8b7c5d475d8a83f64d6513eee5f78d915a7
Thomas Zhang <zhgqthomas@outlook.com>
2025-07-01 08:21:13 +0800

    Add Lark Websocket client logic

--- nodes/FeishuNode/LarkTrigger.node.ts
+++ nodes/FeishuNode/LarkTrigger.node.ts
@@ -15,7 +15,7 @@ export class LarkTrigger implements INodeType {
 		icon: 'file:icon.png',
 		group: ['trigger'],
 		version: 1,
-		description: 'Triggers for Lark events (via long connection)',
+		description: 'Triggers for Lark events',
 		defaults: {
 			name: 'Lark Trigger',
 		},
@@ -45,6 +45,11 @@ export class LarkTrigger implements INodeType {
 						value: '*',
 						description: 'All updates',
 					},
+					{
+						name: 'Callback Query',
+						value: 'callback_query',
+						description: 'Trigger on new incoming callback query',
+					},
 				],
 				required: true,
 				default: [],
@@ -56,12 +61,13 @@ export class LarkTrigger implements INodeType {
 		const credentials = await this.getCredentials('larkCredentialsApi');
 
 		if (!credentials.appid || !credentials.appsecret) {
-			throw new NodeOperationError(this.getNode(), 'Missing required Slack Socket credentials');
+			throw new NodeOperationError(this.getNode(), 'Missing required Lark credentials');
 		}
 		const appId = credentials['appid'] as string;
 		const appSecret = credentials['appsecret'] as string;
 
-		let wsClient = new Lark.WSClient({ appId, appSecret, loggerLevel: Lark.LoggerLevel.info });
+		const client = new Lark.Client({ appId, appSecret });
+		let wsClient: Lark.WSClient | null = new Lark.WSClient({ appId, appSecret, loggerLevel: Lark.LoggerLevel.debug });
 
 		// const eventMap: Record<string, (data: any) => Promise<void>> = {};
 		// for (const eventType of eventTypes) {
@@ -72,19 +78,31 @@ export class LarkTrigger implements INodeType {
 		// }
 
 		const eventDispatcher = new Lark.EventDispatcher({}).register({
-		/**
-		 * 处理用户进入机器人单聊事件
-		 * handle user enter bot single chat event
-		 * https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/im-v1/chat-access_event/events/bot_p2p_chat_entered
-		 */
-		'im.chat.access_event.bot_p2p_chat_entered_v1': async (data) => {
-			this.logger.info(`Lark data:  ${JSON.stringify(data)}`);
-		}
+			'im.message.receive_v1': async (data) => {
+				// console.log(`Received im.message.receive_v1 event: ${JSON.stringify(data)}`);
+				const {
+					message: { chat_id, content },
+				} = data;
+				// 示例操作：接收消息后，调用「发送消息」API 进行消息回复。
+				await client.im.v1.message.create({
+					params: {
+						receive_id_type: 'chat_id',
+					},
+					data: {
+						receive_id: chat_id,
+						content: Lark.messageCard.defaultCard({
+							title: `回复： ${JSON.parse(content).text}`,
+							content: '新年好',
+						}),
+						msg_type: 'interactive',
+					},
+				});
+			},
 		});
 
 		const manualTriggerFunction = async () => {
 			try {
-				wsClient.start({ eventDispatcher });
+				wsClient?.start({ eventDispatcher });
 				this.logger.info('Started Lark app in test mode');
 			} catch (error) {
 				this.logger.error('Error starting Lark app in test mode: ' + error);
@@ -98,7 +116,7 @@ export class LarkTrigger implements INodeType {
 
 		if (this.getMode() === 'trigger') {
 			try {
-				wsClient.start({ eventDispatcher });
+				wsClient?.start({ eventDispatcher });
 				this.logger.info('Started Lark app in trigger mode');
 			} catch (error) {
 				this.logger.error('Error starting Lark app in trigger mode: ' + error);
@@ -108,15 +126,15 @@ export class LarkTrigger implements INodeType {
 
 		const closeFunction = async () => {
 			try {
-				// wsClient = null;
-				this.logger.info('Stopped Lark app');
+				wsClient = null; // Close the WebSocket connection
+				this.logger.info('Lark app has been stopped');
 			} catch (error) {
 				this.logger.error('Error stopping Lark app: ' + error);
 			}
 		};
 		return {
 			closeFunction,
-			manualTriggerFunction
+			manualTriggerFunction,
 		};
 	}
 }
